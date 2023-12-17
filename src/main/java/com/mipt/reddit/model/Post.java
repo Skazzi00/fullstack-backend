2e4c9dc96f39d2568271a2dbcd5fd4700dc351c4
Skazzi00 <eremin.casha@gmail.com>
2023-12-17 17:05:05 +0300

    @AllArgsConstructor @NoArgsConstructor POst

--- src/main/java/com/mipt/reddit/model/Post.java
+++ src/main/java/com/mipt/reddit/model/Post.java
@@ -1,14 +1,18 @@
 package com.mipt.reddit.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name = "posts")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
     @ManyToOne
     User owner;
