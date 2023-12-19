07c436984a7bba3a21875f5c1b441a4d44bf1986
Skazzi00 <eremin.casha@gmail.com>
2023-12-19 03:32:33 +0300

    fix

--- src/main/java/com/mipt/reddit/services/PropertyService.java
+++ src/main/java/com/mipt/reddit/services/PropertyService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PropertyService {
-    PropertyRepository propertyRepository;
+    private final PropertyRepository propertyRepository;
 
     public boolean getNewPostsAllowed() {
         return propertyRepository
