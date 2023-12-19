09a414af9dd3a3e83755addefbfb20e15a8f20c0
Skazzi00 <eremin.casha@gmail.com>
2023-12-19 03:35:09 +0300

    fix

--- src/main/java/com/mipt/reddit/model/Property.java
+++ src/main/java/com/mipt/reddit/model/Property.java
@@ -1,6 +1,7 @@
 package com.mipt.reddit.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "users")
 public class Property {
+    @Id
     String key;
     String value;
 }
